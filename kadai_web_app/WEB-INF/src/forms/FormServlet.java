146b49bfae549cda9ee687bf42e0e3bddb4ea55a
Kento0206 <ken200206@icloud.com>
2025-08-10 22:20:02 +0900

    Add files via upload

---
+++ kadai_web_app/WEB-INF/src/forms/FormServlet.java
@@ -0,0 +1,43 @@
+package forms;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FormServlet extends HttpServlet{
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws IOException, ServletException{
+		
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		Cookie[] cookies = request.getCookies();
+		
+		
+		if(cookies != null) {
+			for(Cookie cookie: cookies) {
+				switch(cookie.getName()) {
+				case "name" -> {
+					request.setAttribute("name", cookie.getValue());
+				}
+				case "email" -> {
+					request.setAttribute("email", cookie.getValue());
+				}
+				case "address" -> {
+					request.setAttribute("address", cookie.getValue());
+				}
+				
+				default -> {}
+				}
+			}
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/pages/formPage.jsp");
+		dispatcher.forward(request, response);
+	}
+}
